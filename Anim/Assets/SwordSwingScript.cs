4599e789c8395d6ce93a4ba1223c80ac5d8e3c00
Th4tOneGuy <49085458+Th4tOneGuy@users.noreply.github.com>
2019-10-23 19:20:20 -0400

    Added spark spawner tester

---
+++ Anim/Assets/SwordSwingScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordSwingScript : MonoBehaviour
+{
+    [SerializeField]
+    int range;
+    [SerializeField]
+    Transform lineStart;
+    [SerializeField]
+    GameObject sparkPrefab;
+
+    void SwingSword()
+    {
+        if (lineStart == null)
+        {
+            Debug.LogError("Did not assign linestart to SwordSwingScript on |" + this.gameObject.name + "| GameObject");
+            return;
+        }
+        RaycastHit hit;
+        if (Physics.Raycast(lineStart.position, lineStart.TransformDirection(Vector3.forward), out hit, range))
+        {
+            Debug.DrawRay(lineStart.position, lineStart.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+            Debug.Log(hit.transform.gameObject.name);
+            Instantiate(sparkPrefab, hit.point, sparkPrefab.transform.rotation);
+        }
+        else
+        {
+            Debug.Log("Did not hit.");
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SwingSword();
+            Debug.Log("Sword was Swung");
+        }
+    }
+}
